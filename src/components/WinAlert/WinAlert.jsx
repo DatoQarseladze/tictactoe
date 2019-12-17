d72563233b94fdcd0363a276da031b5b96a66a6c
dqarseladze <dqarsealdze@gmail.com>
2019-12-17 23:57:23 +0400

    detect click outside of modal

--- src/components/WinAlert/WinAlert.jsx
+++ src/components/WinAlert/WinAlert.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {useRef} from "react";
 import WinStyles from "./WinAlert.module.css";
 
 const WinAlert = ctx => {
+  const modalContainer = useRef(null);
    let test = (event) => {
-       console.log(event.target);
+      console.log(modalContainer.current.contains(event.target));
+      if(!modalContainer.current.contains(event.target)){
+
+      ctx.ctx.setWinner('');
+      }
    }
 
   if (ctx.ctx.state.winner) {
     return (
       <div className={WinStyles.container} onClick={test}>
-        <div className={WinStyles.box}>
+        <div className={WinStyles.box} ref={modalContainer}>
             Winner is {ctx.ctx.state.winner}
         </div>
       </div>
